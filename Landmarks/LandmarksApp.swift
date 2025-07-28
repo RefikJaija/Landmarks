3a93b983217bc363d785ae1e900467f21fdf3996
Refik Jaija <refikjaija3@gmail.com>
2025-07-28 11:29:38 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Refik Jaija on 28.7.25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
